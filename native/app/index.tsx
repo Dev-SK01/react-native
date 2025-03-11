b4147dc5a98680a30417fdf12a058909fe4019e8
devsk <devsk@devsk>
2025-03-11 15:50:58 +0530

    native components , states , textInput

--- native/app/index.tsx
+++ native/app/index.tsx
@@ -1,38 +1,54 @@
-import { Text, View ,FlatList } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet, Button, TextInput } from "react-native";
 
 export default function Index() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+
   return (
     <>
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor:'#000'
-      }}
-    >
-      <Text 
-      style={
-        {color:'#ffff' ,
-        fontSize:16,
-        fontWeight:'bold'}
-        }>Hi, Welcome to React Native !!</Text>
-    </View>
-     <View
-     style={{
-       flex: 1,
-       justifyContent: "center",
-       alignItems: "center",
-       backgroundColor:'#000'
-     }}
-   >
-     <Text 
-     style={
-       {color:'#ffff' ,
-       fontSize:16,
-       fontWeight:'bold'}
-       }>Hi, Welcome to React Native !!</Text>
-   </View>
-   </>
+      <View
+        style={styles.container}
+      >
+        <Text
+          style={styles.text}>
+          Hi, Welcome to React Native !!
+        </Text>
+        <Greetings name='React' />
+        <Greetings name={`React Native ${count}`} />
+        <Text style={{...styles.text,color:'red'}}>{text}</Text>
+        <Button title="Hit Me" onPress={() => setCount(count + 1)} />
+        <TextInput
+          placeholder="Enter Here..."
+          onChangeText={(newText) => setText(newText)}
+          style={styles.text}
+        >
+        </TextInput>
+      </View>
+    </>
   );
+
 }
+
+const Greetings = (props: { name: string }) => {
+  return (
+    <Text style={styles.text} >{props.name}</Text>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#fff'
+  },
+  text: {
+    color: '#000',
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 5
+  }
+})
+
